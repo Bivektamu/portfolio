f8fb5198914dff56fe2c7aedce63950deffe3548
Bivek <bivek.tamu@gmail.com>
2021-06-22 10:36:27 +1000

    Editing readme and config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bivek Jang Gurung | Portfolio`,
+    description: `This is a portfolio website for Bivek Jang Gurung`,
+    author: `@Bivek Jang Gurung`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
